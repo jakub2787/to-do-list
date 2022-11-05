64329538b88ed27f69d256b62a50214c3f02e40a
Jakub Woźny <jakub2787@gmail.com>
2022-11-05 12:45:04 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+{
+    const tasks = [
+        {
+            content: "Pójść na basen",
+            done: false,
+        },
+        {
+            content: "Zjeść obiad",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks ) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    }
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+        
+        form.addEventListener("submit", onFormSubmit);
+
+    };
+
+    init ();
+}
